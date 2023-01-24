8c6bfc7141349d48231493c8f91b66ac2a33b675
olabanji121 <olabanjiabimbola@gmail.com>
2023-01-24 11:22:17 +1100

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,19 @@
+import {surpriseMePrompts} from '../constant'
+import FilSaver from 'file-saver'
+
+export function getRandomPrompt(prompt){
+
+  const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length)
+
+  const randomPrompt  =  surpriseMePrompts[randomIndex]
+
+  if(randomIndex === prompt) return getRandomPrompt(prompt)
+
+  return randomPrompt
+}
+
+
+
+export async function downloadImage(_id,photo){
+  FilSaver.saveAs(photo, `download-${_id}.jpg`)
+}
